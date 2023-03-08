b8f64f5d427a79519267114555a730cea39f6194
its22013 <s22013@std-college.ac.jp>
2023-03-08 17:41:50 +0000

    ok

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+
+const BOARD_SIZE = 3
+
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function Square ({ value, onClick }) {
+  return (
+    <button className='square' onClick={onClick}>
+      {value}
+    </button>
+  )
+}
+
+function Board () {
+  const [squares, setSquares] = useState(Array(BOARD_SIZE ** 2).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  function handleClick (i) {
+    const newSquares = [...squares]
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O'
+    setSquares(newSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  function renderSquare (i) {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />
+  }
+
+  const winner = calculateWinner(squares)
+  const status = winner
+    ? `Winner: ${winner}`
+    : squares.every((square) => square !== null)
+      ? 'Draw'
+      : `Next player: ${xIsNext ? 'X' : 'O'}`
+
+  return (
+    <div>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+export default function Game () {
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+    </div>
+  )
+}
